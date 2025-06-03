3dee6a40bff59289452e24a8cda378490783e228
Dants0 <guilhermedantasgoes@gmail.com>
2025-06-03 16:51:54 -0300

    Done2

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -12,8 +12,8 @@ import { CakesModule } from './cakes/cakes.module';
       // host: 'localhost', // Ou o host do seu serviço (ex.: 'db.supabase.co')
       host: '192.168.7.9', // Ou o host do seu serviço (ex.: 'db.supabase.co')
       port: 5432, // Porta padrão do PostgreSQL
-      username: 'meu_usuario', // Seu usuário
-      password: 'minha_senha', // Sua senha
+      username: 'admin', // Seu usuário
+      password: 'admin', // Sua senha
       database: 'bolos', // Nome do banco
       entities: [__dirname + '/**/*.entity{.ts,.js}'], // Caminho para as entidades
       synchronize: true, // Sincroniza o schema automaticamente (use apenas em desenvolvimento)
